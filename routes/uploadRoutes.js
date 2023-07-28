c1967409e25d4528b3467f0285202b6826442205
Joel.Rocero <joel.rocero@outlook.com>
2023-07-29 00:05:41 +0800

    Change upload path

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -8,10 +8,7 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/')
   },
   filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    )
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
   },
 })
 
@@ -35,7 +32,9 @@ const upload = multer({
 })
 
 router.post('/', upload.single('image'), (req, res) => {
-  res.send(`/${req.file.path}`)
+  const fullUrl = req.protocol + '://' + req.get('host')
+  res.send(`${fullUrl}/${req.file.path}`)
 })
 
 export default router
+
